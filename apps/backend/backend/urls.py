4643367a010cc2a36829f7421cf007ddbe72ec59
FelipeVillota10 <david.villota@correounivalle.edu.co>
2026-03-18 00:52:38 -0500

    Cambio: Se añadieron mas parametros a las URLS

--- apps/backend/backend/urls.py
+++ apps/backend/backend/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # En este apartado van a ir las rutas de las celulas C1,C2,C3,...
 ]
