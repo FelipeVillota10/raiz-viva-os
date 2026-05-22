a5b3bed019980dcf69671f9056816a8c5f7a9612
KappaXVI <montenegro.santiago@correounivalle.edu.co>
2026-05-22 11:59:00 -0500

    Cambios pertinentes version estable

--- apps/backend/backend/settings.py
+++ apps/backend/backend/settings.py
@@ -9,6 +9,8 @@ import dj_database_url
 
 load_dotenv(os.path.join(Path(__file__).resolve().parent.parent, '.env'))
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
