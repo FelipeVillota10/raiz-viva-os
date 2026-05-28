2a98719c48e1a32d3635bf067ef70276e2b6c58c
kmyh <camiloolave220@gmail.com>
2026-05-27 22:31:54 -0500

    cambio de base de datos de prueba a neon

--- apps/backend/backend/settings.py
+++ apps/backend/backend/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:root@localhost:5432/raiz_viva_dev',
+        default='postgresql://neondb_owner:npg_h6lqt9OTDgyQ@ep-red-feather-amf5zv7i-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require',
         conn_max_age=600,
         ssl_require=False
     )
