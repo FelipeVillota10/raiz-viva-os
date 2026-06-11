452814c27ac4e55bbe815e759eee4cf213e7b1d5
FelipeVillota10 <david.villota@correounivalle.edu.co>
2026-06-11 04:11:44 -0500

    Cambio database configuracion a PostgreSQL
    Se actualizo la configuracion de BD para usar postgreSQL en lugar de SQLite.

--- apps/backend/backend/settings.py
+++ apps/backend/backend/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 DATABASES = {
-    #'default': dj_database_url.config(
-    #   default='postgresql://neondb_owner:npg_h6lqt9OTDgyQ@ep-red-feather-amf5zv7i-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require',
-    #    conn_max_age=600,
-    #    ssl_require=False
-
-    #)
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+       default='postgresql://neondb_owner:npg_h6lqt9OTDgyQ@ep-red-feather-amf5zv7i-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require',
         conn_max_age=600,
         ssl_require=False
+
     )
+    #'default': dj_database_url.config(
+    #    default='sqlite:///db.sqlite3',
+    #    conn_max_age=600,
+    #    ssl_require=False
+    #)
 }
 
 
