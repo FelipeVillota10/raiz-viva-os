8680c67b9ebd19743efbf7a020e4adbdc2030d7d
FelipeVillota10 <david.villota@correounivalle.edu.co>
2026-07-02 04:58:28 -0500

    Se activo un modo que permite otros origenes

--- apps/backend/backend/settings.py
+++ apps/backend/backend/settings.py
@@ -217,7 +217,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://raiz-viva-frontend.vercel.app",    
     "https://raiz-viva-frontend-git-*.vercel.app",
 ]
-CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOW_ALL_ORIGINS = True
 
 SESSION_COOKIE_SAMESITE = 'Lax'
 SESSION_COOKIE_HTTPONLY = True
